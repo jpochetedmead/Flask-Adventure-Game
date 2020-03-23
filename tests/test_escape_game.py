591e5af0018e0818268ae9ae4e0a41e29100f3ac
Julio Pochet Edmead <jpe3841@gmail.com>
2020-03-23 18:25:42 -0400

    More changes to the game name...

---
+++ tests/test_escape_game.py
@@ -0,0 +1,9 @@
+from escape_game import create_app
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+def test_index(client):
+    response = client.get('/')
+    assert response.data == b'Flask Heroku Demo'
