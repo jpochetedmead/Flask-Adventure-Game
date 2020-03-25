6748f18c87c79cf5d15b7ca89268d07693e55118
Julio Pochet Edmead <jpe3841@gmail.com>
2020-03-25 07:05:41 -0400

    Update the tess.

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,3 +15,9 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+#
+
+@pytest.fixture
+def runner(app):
+    return app.test_runner()
--- tests/test_escape_game.py
+++ tests/test_escape_game.py
@@ -4,10 +4,21 @@ def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
+# def test_index(client):
+#    response = client.get('/')
+#    assert response.data == b'Flask Heroku Demo'
+
+# def test_hello(client):
+#    response = client.get('/hello')
+#    assert response.data == b'Hello World!'
+
+#### below chekiarlo
+
 def test_index(client):
     response = client.get('/')
-    assert response.data == b'Flask Heroku Demo'
+    assert b'Welcome to Escape The Curse Of Chucky game!' in response.data
+    assert b'Enter Command:' in response.data
 
 def test_hello(client):
     response = client.get('/hello')
-    assert response.data == b'Hello World!'
+    assert b'Hello, World!' in response.data
