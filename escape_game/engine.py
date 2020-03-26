2a89772bea9be146b670f30c07365cea3c1c1d94
Julio Pochet Edmead <jpe3841@gmail.com>
2020-03-26 11:51:02 -0400

    Add more routes.

--- escape_game/engine.py
+++ escape_game/engine.py
@@ -7,9 +7,25 @@ from flask import (
 bp = Blueprint('engine', __name__)
 
 @bp.route('/', methods=('GET', 'POST'))
-def game():
-    return render_template('index.html', message='Welcome to Escape The Curse Of Chucky game!')
+def base():
+    return render_template('base.html', message='Welcome to Escape The Curse Of Chucky game!')
 
 @bp.route('/hello', methods=('GET', 'POST'))
 def hello():
-    return render_template('hello.html', message='Hello, World!')
+    return render_template('game/hello.html', message='Hello, World!')
+
+@bp.route('/index', methods=('GET', 'POST'))
+def index():
+    return render_template('game/index.html')
+
+@bp.route('/menu', methods=('GET', 'POST'))
+def menu():
+    return render_template('game/menu.html')  # TO-DO werkzeug.routing.BuildError:
+
+@bp.route('/guide', methods=('GET', 'POST'))
+def guide():
+    return render_template('game/guide.html')
+
+@bp.route('/profile', methods=('GET', 'POST'))
+def profile():
+    return render_template('game/profile.html')
