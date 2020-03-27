7d0cda8dad61a8ff6b8fa1da3f6333da11ae4e1e
Julio Pochet Edmead <jpe3841@gmail.com>
2020-03-27 14:00:45 -0400

    Add more tests.

--- tests/test_escape_game.py
+++ tests/test_escape_game.py
@@ -1,4 +1,4 @@
-from escape_game import *
+from escape_game import create_app
 
 def test_import():
     assert create_app
@@ -11,17 +11,56 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'In this game your goal is to escape from Chucky, or at least survive as long as you can. Have fun!' in response.data
+    assert b'<input type="submit"' in response.data
+
 
 def test_hello(client):
     response = client.get('/hello')
+    assert response.status_code == 200
     assert b'Hello, World!' in response.data
 
-####################################
-######### TESTING FOR PRACTICE
-#def test_greeting_page(client):
-#    response1 = client.get('/greet/Amy')
-#    assert b'Hello Amy' in response1.data
+def test_menu(client):
+    response = client.get('/menu/<name>')
+    assert response.status_code == 200
+    #assert b'<p>{{ person }}, please choose a menu option to proceed:</p>' in response.data
 
-#    response2 = client.get('/greet/Bob')
-#    assert b'Hello Amy' not in response2.data
-#    assert b'Hello Bob' in response2.data
+def test_guide(client):
+    response = client.get('/guide')
+    assert response.status_code == 200
+
+def test_profile(client):
+    response = client.get('/profile')
+    assert response.status_code == 200
+
+def test_room(client):
+    response = client.get('/room')
+    assert response.status_code == 200
+
+def test_room1(client):
+    response = client.get('/room1')
+    assert response.status_code == 200
+
+def test_room2(client):
+    response = client.get('/room2')
+    assert response.status_code == 200
+
+def test_room3(client):
+    response = client.get('/room3')
+    assert response.status_code == 200
+
+def test_room4(client):
+    response = client.get('/room4')
+    assert response.status_code == 200
+
+def test_room5(client):
+    response = client.get('/room5')
+    assert response.status_code == 200
+
+# Need to fix these tests below:
+#def test_register(client):
+#    response = client.get('/register')
+#    assert response.status_code == 200
+
+#def test_login(client):
+#    response = client.get('/login')
+#    assert response.status_code == 200
