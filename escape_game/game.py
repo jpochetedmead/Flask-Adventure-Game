4e3029642f130d92e37639dd4672d00b6ee5da67
Julio Pochet Edmead <jpe3841@gmail.com>
2020-03-27 14:00:11 -0400

    Remove non-needed files.

--- escape_game/game.py
+++
@@ -1,5 +0,0 @@
-from escape import *
-from rooms import *
-game = escape.loading_game()
-#game = loading_game()
-#game.play()
--- get_ip.py
+++
@@ -1,4 +0,0 @@
-import requests
-response = requests.get('https://httpbin.org/ip')
-data = response.json()
-print(f"Your IP is {data['origin']}")
