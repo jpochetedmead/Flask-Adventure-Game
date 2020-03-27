008c52cd51c2cc37d815aca26f91464ff43730ab
Julio Pochet Edmead <jpe3841@gmail.com>
2020-03-27 02:30:08 -0400

    Update tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,9 +15,3 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
-
-#
-
-@pytest.fixture
-def runner(app):
-    return app.test_runner()
--- tests/test_escape_game.py
+++ tests/test_escape_game.py
@@ -1,24 +1,27 @@
-from escape_game import create_app
+from escape_game import *
+
+def test_import():
+    assert create_app
 
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
 
-# def test_index(client):
-#    response = client.get('/')
-#    assert response.data == b'Flask Heroku Demo'
-
-# def test_hello(client):
-#    response = client.get('/hello')
-#    assert response.data == b'Hello World!'
-
-#
-
 def test_index(client):
     response = client.get('/')
-    assert b'Welcome to Escape The Curse Of Chucky game!' in response.data
-    assert b'Enter Command:' in response.data
+    assert response.status_code == 200
+    assert b'In this game your goal is to escape from Chucky, or at least survive as long as you can. Have fun!' in response.data
 
 def test_hello(client):
     response = client.get('/hello')
     assert b'Hello, World!' in response.data
+
+####################################
+######### TESTING FOR PRACTICE
+#def test_greeting_page(client):
+#    response1 = client.get('/greet/Amy')
+#    assert b'Hello Amy' in response1.data
+
+#    response2 = client.get('/greet/Bob')
+#    assert b'Hello Amy' not in response2.data
+#    assert b'Hello Bob' in response2.data
